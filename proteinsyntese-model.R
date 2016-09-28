12704ac24fde1d6377b78d56a1a0246f33778d9b
Marcus Medom Ryding <mrok4a@gmail.com>
2016-09-28 16:27:00 +0200

    Lillebitte stavefejl

--- proteinsyntese-model.R
+++ proteinsyntese-model.R
@@ -52,7 +52,7 @@ for(t in t0:t_max)
   proteinSyntetiseret[t+1] = proteinSyntetiseret[t] + DproteinSyntetiseret[t]
 }
 plot(t0:(t_max+1),proteinSyntetiseret,type = "l",
-     ylab = "Massen af protein i gram",xlab = "Tiden i sekunder",main = "Massen af protien over tid",
+     ylab = "Massen af protein i gram",xlab = "Tiden i sekunder",main = "Massen af protein over tid",
      mgp=c(1.4,.4,0), font.lab=2,
      cex.lab=0.9, cex.axis=0.8,cex.main=1.2)
 (10^-9*10^-3)/proteinSyntetiseret[t_max] # Enheden er milliarder af celler der skal til at producere 1 mg protein på en time
