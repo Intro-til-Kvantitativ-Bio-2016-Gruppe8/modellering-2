38ac11b3004eab250d2a6d4fd1a687249205b857
Marcus Medom Ryding <mrok4a@gmail.com>
2016-09-28 13:41:35 +0200

    Rettede konstanter og enheder

--- proteinsyntese-model.R
+++ proteinsyntese-model.R
@@ -13,9 +13,9 @@ proteinMasse = function(antalAminosyrer)
   antalAminosyrer*118.9
 }
 
-maengde_mRNA = 4.3E-10
+maengde_mRNA = 3.321E-19
 maengde_ribosomer = 9.98E-18
-maengde_tRNA = 6.23E-19
+maengde_tRNA = 12.2*maengde_ribosomer
 ribosomalHastighed = 4 # aminosyrer/sek
 
 # Model
@@ -29,4 +29,4 @@ for(t in t0:t_max)
   proteinSyntetiseret[t+1] = proteinSyntetiseret[t] + proteinSyntese[t]
 }
 plot(t0:(t_max+1),proteinSyntetiseret,type = "l")
-proteinSyntetiseret[t_max]
+(10^-9*10^-3)/proteinSyntetiseret[t_max] # Enheden er milliarder af celler der skal til at producere 1 mg protein på en time
